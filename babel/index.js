98d921308d385ad76d811436304932a2c7d36770
Katherine Donnally <kbd3ax@virginia.edu>
2019-05-13 13:36:52 -0400

    Add helper function to calculate winner

--- babel/index.js
+++ babel/index.js
@@ -80,3 +80,25 @@ ReactDOM.render(
 	<Game />,
 	document.getElementById('root')
 );
+
+// ========================================
+
+// 1. Declare winner function
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i=0; i < lines.length; i++) {
+		const [a, b, c] = lines[i]; // didn't know you could do this syntax!
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a]; // winner is whatever is in that square
+		}
+	}
+	return null; // else
+}
