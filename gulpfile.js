cf3eae79d99b9b9e5939c06d8d3e36951a41b78b
Katherine Donnally <kbd3ax@virginia.edu>
2019-05-20 16:18:48 -0400

    Add gulp-clean package & cleaner functions to tasks
    - Was accidentally racking up files that I didn't want &
      suddenly understood why people used the 'clean' functions last
      time!
    
    - It's also to guarantee the package updates your files - gulp babel
      wasn't doing that; I guess you have to explicitly program it to

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean');
 
 // ================== CSS: ====================
 
-var cssFormat = (done) => {
+const cssFormat = (done) => {
 	gulp.src('css/style.scss')
 		.pipe(sass({
 			includePaths: ['./css/_sass'],
@@ -14,14 +16,22 @@ var cssFormat = (done) => {
 	done();
 };
 
+const cssClean = (done) => {
+	gulp.src('css/style.css', {allowEmpty: true})
+			.pipe(clean());
+	done();
+};
+
 gulp.task('css:format', cssFormat);
 
 gulp.task('css:watch', () => {
 	return gulp.watch('css/**/*.scss', cssFormat);
 });
 
+gulp.task('css:clean', cssClean);
+
 // gulp css
-gulp.task('css', gulp.series('css:format', 'css:watch'));
+gulp.task('css', gulp.series('css:clean', 'css:format', 'css:watch'));
 
 // ================== Babel: ==================
 
@@ -30,7 +40,14 @@ const babelCompile = (done) => {
 		.pipe(babel({
 			presets: ["@babel/preset-react"]
 		}))
+		.pipe(concat('main.js'))
 		.pipe(gulp.dest('js'))
+	done();
+};
+
+const babelClean = (done) => {
+	gulp.src('js/*.js', {allowEmpty: true})
+		.pipe(clean());
 	done();
 };
 
@@ -40,8 +57,11 @@ gulp.task('babel:watch', () => {
 	return gulp.watch('babel/*.js', babelCompile)
 });
 
+gulp.task('babel:clean', babelClean);
+
 // gulp babel
-gulp.task('babel', gulp.series('babel:compile', 'babel:watch'));
+gulp.task('babel', gulp.series('babel:clean', 'babel:compile', 'babel:watch'));
+
 
 // ================== Default: ===================
 
